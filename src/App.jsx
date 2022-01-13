03085f8d1e4e21d171836fc5a51e378241f5d3eb
satyam kushwaha <satyam21@navgurukul.org>
2022-01-13 17:29:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+
+
+function App() {
+    const [change, setChange] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        phone: ""
+    }); 
+
+    const Submit = (event) => {
+        event.preventDefault();
+        alert('sumbmited succesfully')
+    };
+
+    const fun_onChange = (e) => {
+        const {value, name} = e.target
+
+        setChange((preValue) => {
+            if (name === "fname") {
+                return {
+                    fname: value,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: preValue.phone
+                }
+            }else if (name === "lname") {
+                return {
+                    fname: preValue.fname,
+                    lname: value,
+                    email: preValue.email,
+                    phone: preValue.phone
+                }
+            }
+            else if (name === "email") {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone: preValue.phone
+                }
+            }
+            else if (name === "phone") {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: value,
+                }
+            }
+        })
+
+
+    }
+
+
+    return (
+        <>
+            <div className='main_container'>
+                <h3>Hello {change.fname}  {change.lname}</h3>
+                <p>{change.email}</p>
+                <p>{change.phone}</p>
+                <form onSubmit={ Submit} >
+                    <input
+                        name='fname'
+                        value={change.fname}
+                        type="text"
+                        onChange={fun_onChange}
+                        placeholder='enter first name'
+                    />
+
+                    <input
+                        name='lname'
+                        value={change.lname}
+                        type="text"
+                        onChange={fun_onChange}
+                        placeholder='enter last name'
+                    />
+
+                    <input
+                        name='email'
+                        value={change.email}
+                        type="text"
+                        onChange={fun_onChange}
+                        placeholder='enter email'
+                    />
+
+                    <input
+                        name='phone'
+                        value={change.phone}
+                        type="text"
+                        onChange={fun_onChange}
+                        placeholder='enter phone number'
+                    />
+
+                    <input type="submit" />
+                </form>
+            </div>
+
+        </>
+    )
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import reactDom from "react-dom";
+import './index.css'
+
+import App from "./App"
+
+reactDom.render(
+    <App/>,
+    document.getElementById("root")
+)
